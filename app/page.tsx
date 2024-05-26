969f88a15827f6cc61dd6f1e8d89a11a677f47a4
SonDangUIT1111 <sondanguit@gmail.com>
2024-05-26 17:16:50 +0700

    Fix UI payment page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
-import { Button, Card, CardBody, CardFooter, CardHeader, Image, Input } from '@nextui-org/react';
-import { useUser } from '@/hooks/useUsers';
-import { usePayment } from '@/hooks/usePayment';
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Image,
+  Input,
+} from "@nextui-org/react";
+import { useUser } from "@/hooks/useUsers";
+import { usePayment } from "@/hooks/usePayment";
 import { useRouter } from "next/navigation";
-import Header from './(components)/Header';
-
+import Header from "./(components)/Header";
+import Footer from "./(components)/Footer";
 
 const CURRENCY_FORMAT = new Intl.NumberFormat(undefined, {
   currency: "VND",
   style: "currency",
 });
-
 
 export function formatCurrency(value: number) {
   return CURRENCY_FORMAT.format(value);
@@ -20,10 +27,15 @@ export function formatCurrency(value: number) {
 
 const Page = () => {
   const { fetchUsersInfoByPhoneNumber } = useUser();
-  const { uploadVNPAYPaymentInfo, uploadZaloPaymentInfo, getSkyCoinPackage, createPaymentHistory } = usePayment();
+  const {
+    uploadVNPAYPaymentInfo,
+    uploadZaloPaymentInfo,
+    getSkyCoinPackage,
+    createPaymentHistory,
+  } = usePayment();
   const [isLoading, setisLoading] = useState(true);
   const router = useRouter();
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState("");
   const [userInfo, setUserInfo] = useState(null);
   const [packages, setPackages] = useState([]);
   const [currentDateTime, setCurrentDateTime] = useState("");
@@ -36,10 +48,9 @@ const Page = () => {
       setUserInfo(res); // Assuming the response contains user information
       console.log(res); // You can remove this line if you don't need to log the information
     } catch (error) {
-      console.error('Error fetching user information:', error);
+      console.error("Error fetching user information:", error);
     }
   };
-
 
   const onPaymentVNPay = async () => {
     const selectedPackage = packages[selectedPackageIndex];
@@ -47,10 +58,10 @@ const Page = () => {
       userId: userInfo.id,
       orderDate: currentDateTime,
       paymentMethod: "VNPay",
-      status: 'pending',
+      status: "pending",
       price: selectedPackage.price,
       packageId: selectedPackage._id,
-    }
+    };
 
     const createOrderSuccess = await createPaymentHistory(createOrder);
     console.log(createOrderSuccess);
@@ -64,19 +75,18 @@ const Page = () => {
 
     const uploadPaymentSuccess = await uploadVNPAYPaymentInfo(VNPAY);
     router.push(uploadPaymentSuccess.data);
-  }
+  };
 
   const onPaymentZaloPay = async () => {
-
     const selectedPackage = packages[selectedPackageIndex];
     const createOrder = {
       userId: userInfo.id,
       orderDate: currentDateTime,
       paymentMethod: "VNPay",
-      status: 'pending',
+      status: "pending",
       price: selectedPackage.price,
       packageId: selectedPackage._id,
-    }
+    };
 
     const createOrderSuccess = await createPaymentHistory(createOrder);
     console.log(createOrderSuccess);
@@ -88,12 +98,12 @@ const Page = () => {
 
     const uploadPaymentSuccess = await uploadZaloPaymentInfo(ZaloPay);
     router.push(uploadPaymentSuccess.data.response.order_url);
-  }
+  };
 
   const onPurchase = async (index: number | React.SetStateAction<null>) => {
     // Logic for purchasing the packageItem goes here
     setSelectedPackageIndex(index);
-    console.log('Purchased:', packages[index]);
+    console.log("Purchased:", packages[index]);
     setIsPackageSelected(true); // Set the boolean to true when a package is selected
   };
 
@@ -116,67 +126,117 @@ const Page = () => {
   }, []);
 
   function clearUserData() {
-    setPhoneNumber('');
+    setPhoneNumber("");
     setUserInfo(null);
     setSelectedPackageIndex(null);
     setIsPackageSelected(false);
   }
 
   return (
-    <div className='w-full h-full bg-slate-50'>
+    <div className="w-full h-full bg-slate-50">
       <Header />
-      <div className='w-full h-full min-h-screen flex flex-col items-center justify-center'>
-        <div className="w-full max-w-screen-md bg-white rounded-md shadow-sm p-14 mb-8">
+      <div className="w-full h-full flex flex-col items-center justify-center mb-8">
+        <img className="h-auto w-full" src="./welcomebanner2.png" alt="" />
+        <div className="w-full max-w-screen-md bg-white rounded-lg shadow-lg md:-mt-10 p-14 mb-8">
           {userInfo ? (
             <>
-              <div className='flex flex-row justify-between'>
-                <h1 className="font-bold mb-2">1. Nhập số điện thoại của bạn</h1>
-                <a onClick={() => clearUserData()} className='cursor-pointer'>Lựa chọn tài khoản khác</a>
+              <div className="flex flex-row justify-between">
+                <div className="flex flex-row gap-1 mb-3">
+                  <h1 className="font-semibold text-fuchsia-500">1.</h1>
+                  <h1 className="font-semibold">Nhập số điện thoại của bạn</h1>
+                </div>
+                <a
+                  onClick={() => clearUserData()}
+                  className="cursor-pointer font-semibold text-violet-500 text-[14px]"
+                >
+                  Đổi tài khoản
+                </a>
               </div>
-              <div className='border-dashed border-2 border-blue-400 bg-blue-100 p-4'>
-                <p className='text-blue-400'>Họ và tên:  <span className='text-black'>{userInfo.username!}</span></p>
+              <div className="border-dashed border-2 rounded-sm border-blue-400 bg-blue-50 p-4">
+                <p className="text-blue-400 text-[14px] font-semibold mb-2">
+                  Họ và tên:{" "}
+                  <span className="text-black text-[13px] font-normal">
+                    {userInfo.username!}
+                  </span>
+                </p>
+                <p className="text-emerald-500 font-semibold text-[14px]">
+                  UserId:{" "}
+                  <span className="text-gray-500 font-normal text-[12px]">
+                    {userInfo.id!}
+                  </span>
+                </p>
                 {/* <p>ID: {userInfo.id!}</p> */}
               </div>
             </>
           ) : (
             <>
-              <h1 className="font-bold mb-2">1. Nhập số điện thoại của bạn</h1>
-              <div className='flex flex-row'>
+              <div className="flex flex-row gap-1 mb-3">
+                <h1 className="font-semibold text-fuchsia-500">1.</h1>
+                <h1 className="font-semibold">Nhập số điện thoại của bạn</h1>
+              </div>
+              <div className="flex flex-col">
                 <Input
-                  className='w-full'
+                  className="w-full h-[50px] text-sm"
                   type="number"
-                  label="Số điện thoại"
-                  placeholder="Nhập số điện thoại của bạn"
+                  radius="sm"
+                  size="md"
+                  variant="bordered"
+                  placeholder="Số điện thoại đăng ký"
                   value={phoneNumber}
                   onChange={(e) => setPhoneNumber(e.target.value)}
                 />
+                <div className="flex flex-row gap-1">
+                  <p className="text-[12px] font-semibold text-gray-800">
+                    Lưu ý:
+                  </p>
+                  <p className="text-[12px] text-gray-600">
+                    Nhập số điện thoại đã đăng ký tài khoản skylark
+                  </p>
+                </div>
               </div>
-              <Button className='w-full mt-4 bg-blue-400 text-white font-bold' onClick={handleButtonClick}>Xác nhận</Button>
+              <div className="w-full flex justify-center">
+                <Button
+                  className="w-1/2 h-[40px] rounded-md mt-4 bg-blue-400 text-white font-bold"
+                  onClick={handleButtonClick}
+                >
+                  Xác nhận
+                </Button>
+              </div>
             </>
           )}
         </div>
 
         {userInfo ? (
-          <div className="w-full max-w-screen-md bg-white rounded-md shadow-sm p-6 md:p-14 mb-8">
-            <h1 className="text-xl font-bold mb-4">2. Chọn gói cần nạp</h1>
-            <div className='flex flex-wrap -mx-4'>
+          <div className="w-full max-w-screen-md bg-white rounded-lg shadow-lg p-6 md:p-14 mb-8">
+            <div className="flex flex-row gap-1 mb-3">
+              <h1 className="font-semibold text-fuchsia-500">2.</h1>
+              <h1 className="font-semibold">Chọn gói nạp skycoin</h1>
+            </div>
+            <div className="flex flex-wrap -mx-4">
               {packages.map((packageItem, index) => (
                 <div
                   key={index}
-                  className={`w-full sm:w-1/2 md:w-1/3 px-4 p-4 mb-4 cursor-pointer ${selectedPackageIndex === index ? 'ring-2 ring-blue-500' : 'hover:ring-1 hover:ring-gray-300'}`}
+                  className={`w-1/2 sm:w-1/3 md:w-1/4 p-2 cursor-pointer rounded-md ${
+                    selectedPackageIndex === index
+                      ? "ring-2 ring-emerald-400"
+                      : "hover:ring-1 hover:ring-blue-500"
+                  }`}
                   onClick={() => onPurchase(index)}
                 >
-                  <div className="relative bg-gray-100 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200">
-                    <div className="flex items-center justify-between mb-2">
-                      <h2 className="text-lg font-semibold">{packageItem.packageName}</h2>
-                      {/* {selectedPackageIndex === index && (
-                        <RiCheckboxCircleFill className="text-blue-500" size={24} />
-                      )} */}
-                    </div>
+                  <div className="relative bg-slate-50 p-3 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200">
                     <div className="flex flex-col items-center">
-                      <Image src={'/skycoin.png'} width={100} height={100} alt="Skycoin" />
-                      <p className="mt-2">Số lượng: {packageItem.quantity}</p>
-                      <p>Giá tiền: {formatCurrency(packageItem.price)}</p>
+                      <Image
+                        src={"/coinpackage.png"}
+                        width={100}
+                        height={100}
+                        alt="Skycoin"
+                      />
+                      <p className="mt-2 font-medium text-gray-600 text-[12px] sm:text-[13px] md:text-[10px]">
+                        {packageItem.packageName}
+                      </p>
+                      <p className="font-semibold text-black text-[16px] sm:text-[16px] md:text-[14px]">
+                        {formatCurrency(packageItem.price)}
+                      </p>
                     </div>
                   </div>
                 </div>
@@ -186,23 +246,38 @@ const Page = () => {
         ) : null}
 
         {isPackageSelected ? (
-          <div className="w-full max-w-screen-md bg-white rounded-md shadow-sm p-14">
-            <h1 className="font-bold mb-2">3. Chọn phương thức thanh toán</h1>
-            <p className='font-medium mb-2'>Bạn sẽ được điều hướng đến trang thanh toán của đối tác cung cấp dịch vụ.</p>
-            <div className='flex flex-row row-span-2 gap-3'>
-              <Button onClick={onPaymentVNPay} className='bg-blue-400 text-white'>
-                VNPay
+          <div className="w-full max-w-screen-md bg-white rounded-lg shadow-lg p-14">
+            <div className="flex flex-row gap-1 mb-1">
+              <h1 className="font-semibold text-fuchsia-500">3.</h1>
+              <h1 className="font-semibold">Chọn hình thức thanh toán</h1>
+            </div>
+            <div className="flex flex-row gap-1 mb-4">
+              <p className="text-[12px] text-gray-600">
+                <span className="text-[12px] font-semibold text-gray-800">
+                  Lưu ý:{" "}
+                </span>
+                Bạn sẽ được điều hướng đến trang thanh toán sau khi chọn.
+              </p>
+            </div>
+            <div className="flex flex-row row-span-2 gap-6">
+              <Button
+                onClick={onPaymentVNPay}
+                className="bg-blue-400 h-20 w-20 p-0 shadow-md hover:scale-105 transition ease-in-out duration-300 text-white"
+              >
+                <img className="h-20 w-20" src="./vnpay.jpg" alt="" />
               </Button>
-              <Button onClick={onPaymentZaloPay} className='bg-orange-400 text-white'>
-                ZaloPay
+              <Button
+                onClick={onPaymentZaloPay}
+                className="bg-orange-400 h-20 w-20 p-0 shadow-md hover:scale-105 transition ease-in-out duration-300 text-white"
+              >
+                <img className="h-20 w-20" src="./zalopay.png" alt="" />
               </Button>
             </div>
-
           </div>
         ) : null}
-
       </div>
-    </div >
+      <Footer />
+    </div>
   );
 };
 
